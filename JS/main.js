e65f63b3c2dc4e43a66eb7e0b719e9e6c0784211
sangmin131 <smha77@naver.com>
2022-12-21 18:10:34 +0900

    뼈대 완성 반응형 제작중

--- JS/main.js
+++ JS/main.js
@@ -10,5 +10,17 @@ new Swiper('.promotion .swiper', {
     el: '.promotion .swiper-pagination', // 페이비 번호 요소
     clickable: true // 사용자의 페이지 번호 제어 여부
   },
+  navigation: { //이전 다음 슬라이드 버튼 사용
+    nextEl: '.promotion .swiper-button-next',
+    prevEl: '.promotion .swiper-button-prev',
+  },
 
+});
+const btnHamburger = document.querySelector('.btn-hamburger');
+const navEl = document.querySelector('.lnb');
+const btnGroup = document.querySelector('.gnd > div');
+
+btnHamburger.addEventListener('click', function(){
+    navEl.classList.toggle('active');
+    btnGroup.classList.toggle('active');
 });
