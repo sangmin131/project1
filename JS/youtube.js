cea9f57294100a37d91b1c2d4e583077ee443bbc
sangmin131 <smha77@naver.com>
2022-12-23 16:53:59 +0900

    1+1페이지 제작 및 필름상세페이지 제작중
    1+1페이지 사진 및 상세설명 , 필름상세페이지 유튜브 까지 업로드

---
+++ JS/youtube.js
@@ -0,0 +1,29 @@
+     // youtube Iframe API 비동기로 로드
+    //  api를 사용하지 않고 Iframe 태그만 써서 영상 삽입이 가능한데 태그 속성만으로 커스텀하기에 한계가 있음
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+   //  var player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('player', {
+       //  height: '360',
+       //  width: '640',
+        videoId: 'Wq40pwsC5Vw',//최초 재생할 유튜브영상 ID
+       playerVars:{//자세한 옵션은 플레이어 매개변수 메뉴 확인
+           autoplay:true,//자동 재생 유무
+           loop:true, //반복 재생유무
+           playlist:'Wq40pwsC5Vw'//반복 재생할 유튜브영상 ID목록
+       },
+        events: {
+           //영상 준비되었을 때
+           onReady:function(event){
+               event.target.mute();//음소거!
+           }
+        }
+      });
+    }
