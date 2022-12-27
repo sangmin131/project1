08b831e6a5ede192e63fec1697ec4343fca9ac5a
sangmin131 <smha77@naver.com>
2022-12-27 18:17:03 +0900

    필름 상세페이지 반응형 작업중

--- JS/youtube.js
+++ JS/youtube.js
@@ -24,3 +24,6 @@
       }
       });
     };
+    document.cookie = "safeCookie1=foo; SameSite=Lax"; 
+    document.cookie = "safeCookie2=foo"; 
+    document.cookie = "crossCookie=bar; SameSite=None; Secure";
